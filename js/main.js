518b12537ac586ce38d3330a014049284180d93d
Steven Maguire <stevenmaguire@gmail.com>
2017-02-11 14:07:38 -0600

    Add payment processing

--- js/main.js
+++ js/main.js
@@ -10,9 +10,33 @@
         token: function(token) {
             // Use the token to create the charge with a server-side script.
             // You can access the token ID with `token.id`
+            var tokenUuid = 'token-' + token.email + '-' + token.id;
             if (typeof ga == 'function') {
-                ga('send', 'event', 'checkout', 'completed', 'token-' + token.email + '-' + token.id, {{ site.pizza_price_cents }});
+                ga('send', 'event', 'checkout', 'tokenCreated', tokenUuid, {{ site.pizza_price_cents }});
             }
+            var http_request;
+            var params = { 'token': token.id, 'email': token.email, 'amount': {{ site.pizza_price_cents }} };
+            http_request = new XMLHttpRequest();
+            http_request.onreadystatechange = function () {
+                var response = null;
+                if (http_request.responseText) {
+                    response = JSON.parse(http_request.responseText);
+                }
+                if (http_request.readyState == 4) {
+                    if (http_request.status == 200) {
+                        if (response && typeof ga == 'function') {
+                            ga('send', 'event', 'checkout', 'complete', response.id, response.amount);
+                        }
+                    } else {
+                        if (response && typeof ga == 'function') {
+                            ga('send', 'event', 'checkout', 'error', JSON.stringify({'request': params, 'response': response}));
+                        }
+                    }
+                }
+            };
+            http_request.open('{{site.payment_gateway_method}}', '{{site.payment_gateway_url}}');
+            http_request.setRequestHeader("Content-Type", "application/json");
+            http_request.send(JSON.stringify(params));
         },
         opened: function () {
             if (typeof ga == 'function') {
@@ -98,3 +122,4 @@
     });
 
 })(jQuery);
+//
